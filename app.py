5c13e3be294107029bc97b343fedeca017f23305
RamonLull <117016942+RamonLull@users.noreply.github.com>
2023-04-23 20:21:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ from flask import Flask, render_template, request, redirect
 import lyricsgenius
 
 app = Flask(__name__)
-word, artist, search_result = None, None, None
-
 
 
 @app.route('/')
 def index():
-    return "welcome"
     return render_template('index.html')
 
 
-@app.route('/inquiry', methods=['POST','GET'])
+@app.route('/inquiry', methods=['POST', 'GET'])
 def check_artist_name():
     if request.form['submit_button'] == 'Evet':
         # create an empty list to store the lyrics
@@ -34,11 +31,11 @@ def check_artist_name():
         return "lol"
 
 
-@app.route('/get_lyrics', methods=['POST'])
+@app.route('/', methods=['POST'])
 def get_lyrics():
-    global search_result, artist, word
     # get the input values from the HTML form
     artist = request.form['artist']
+    album = request.form['album']
     word = request.form['word']
     access_token = "vBzal6SKsuLALoUsRkUZDqXxwD7ooJtUi4RgMcfoAOImsYlGA_emercol_H_F__N"
 
@@ -46,8 +43,23 @@ def get_lyrics():
     genius = lyricsgenius.Genius(access_token)
 
     # search for the artist
-    search_result = genius.search_artist(artist, max_songs=1)
-    return render_template('inquiry.html', artist_name=search_result.name)
+    candidate = genius.search_album(name=album, artist=artist)
+    artist = genius.search_artist(artist_name=artist, max_songs=1, artist_id=candidate.artist.id)
+    # create an empty list to store the lyrics
+    lyrics_list = []
+
+    # loop through each song by the artist
+    for song in artist.songs:
+        # check if the lyrics contain the specific word
+        if word in song.lyrics:
+            # add the lyrics to the list
+            lyrics_list.append(song.lyrics)
+
+    # join the lyrics in the list into a single string separated by two newlines
+    lyrics = '\n\n'.join(lyrics_list)
+
+    # render the lyrics in the HTML template
+    return render_template('index.html', lyrics=lyrics)
 
 
 if __name__ == '__main__':
